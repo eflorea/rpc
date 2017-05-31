bae503afbd5e1d8be8bc3756f1e870ef99fba9d0
Eduard Florea <edward.florea@gmail.com>
2017-05-31 14:58:12 -0700

    add ability to disable showing of errors

--- src/RPC/init.php
+++ src/RPC/init.php
@@ -1,11 +1,23 @@
 <?php
 
+function RPC_Shutdown() { 
+    echo 'Something went wrong. Our amazing team of developers have been notified. Please try again later.';
+}
+register_shutdown_function( 'RPC_Shutdown' );
+
 $dotenv = new \Dotenv\Dotenv( ROOT_PATH . '/config' );
 $dotenv->load();
 
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+error_reporting( E_ALL );
+ini_set( 'display_errors', 0 );
+
+if( getenv( "SHOW_ERRORS" ) === "true" )
+{
+	ini_set( 'display_errors', 1 );
+	$whoops = new \Whoops\Run;
+	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+	$whoops->register();
+}
 
 //set some default constants if they aren't defined
 if( ! defined( 'APP_PATH' ) )
@@ -18,11 +30,5 @@ if( ! defined( 'CACHE_PATH' ) )
 	define( 'CACHE_PATH', ROOT_PATH . '/tmp/cache' );
 }
 
-/**
- * Keeps history of queries in current call.
- * Can be referenced via $model->lastQuery() or via
- * $model->queries array
- */
-define( 'DEBUG_QUERIES', getenv( 'DEBUG_QUERIES' ) );
 
 ?>
