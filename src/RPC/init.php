13ee820336b8208a9f0fd28e48c72daf427ac93d
Justin Welenofsky <welenofsky@gmail.com>
2017-12-06 12:00:36 -0800

    Add CLI plain text error handler

--- src/RPC/init.php
+++ src/RPC/init.php
@@ -1,9 +1,9 @@
 <?php
 
 function RPC_Shutdown() { 
-	if( count( func_get_args() ) )
-	{
-    	echo 'Something went wrong. Our amazing team of developers have been notified. Please try again later.';
+    if( count( func_get_args() ) )
+    {
+        echo 'Something went wrong. Our amazing team of developers have been notified. Please try again later.';
     }
 }
 register_shutdown_function( 'RPC_Shutdown' );
@@ -16,21 +16,25 @@ ini_set( 'display_errors', 0 );
 
 if( getenv( "SHOW_ERRORS" ) === "true" )
 {
-	ini_set( 'display_errors', 1 );
-	$whoops = new \Whoops\Run;
-	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-	$whoops->register();
+    ini_set( 'display_errors', 1 );
+    $whoops = new \Whoops\Run;
+    if( strpos( php_sapi_name(), 'cli' ) === false ) {
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    } else {
+        $whoops->pushHandler(new \Whoops\Handler\PlainTextHandler);
+    }
+    $whoops->register();
 }
 
 //set some default constants if they aren't defined
 if( ! defined( 'APP_PATH' ) )
 {
-	define( 'APP_PATH', ROOT_PATH . '/APP' );
+    define( 'APP_PATH', ROOT_PATH . '/APP' );
 }
 
 if( ! defined( 'CACHE_PATH' ) )
 {
-	define( 'CACHE_PATH', ROOT_PATH . '/tmp/cache' );
+    define( 'CACHE_PATH', ROOT_PATH . '/tmp/cache' );
 }
 
 
